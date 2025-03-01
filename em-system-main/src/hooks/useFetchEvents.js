6b5ec5cb604273ed9506371b4db61706659b5b05
Nicknjano <122860272+Nicknjano@users.noreply.github.com>
2025-03-01 13:31:38 +0300

    Delete em-system-main directory

--- em-system-main/src/hooks/useFetchEvents.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { EventContext } from "../context/EventContext";
-
-// Custom Hook for Fetching Events
-const useFetchEvents = () => {
-  return useContext(EventContext);
-};
-
-export default useFetchEvents;
--- em-system-main/tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-// filepath: /c:/Users/USER/Downloads/em-system-main/em-system-main/tailwind.config.cjs
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
